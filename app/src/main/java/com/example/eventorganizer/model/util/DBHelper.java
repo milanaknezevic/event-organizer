74efb09b8ada006aa7c17761f09f7bd8dc0a9eb6
Milana <milanaknezevicc98@gmail.com>
2023-10-07 12:55:00 +0200

    first commit

---
+++ app/src/main/java/com/example/eventorganizer/model/util/DBHelper.java
@@ -0,0 +1,27 @@
+package com.example.eventorganizer.model.util;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME="spain_data.db";//?
+    public static final Integer DATABASE_VERSION=1;
+
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
