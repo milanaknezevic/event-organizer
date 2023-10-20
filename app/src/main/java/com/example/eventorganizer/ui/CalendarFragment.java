bda4e5c60c0fa549789328d14c8796fc0b9928fb
Milana <milanaknezevicc98@gmail.com>
2023-10-20 22:03:42 +0200

    calendar fixed

--- app/src/main/java/com/example/eventorganizer/ui/CalendarFragment.java
+++ app/src/main/java/com/example/eventorganizer/ui/CalendarFragment.java
@@ -1,13 +1,6 @@
 package com.example.eventorganizer.ui;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
-
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +8,10 @@ import android.widget.AdapterView;
 import android.widget.CalendarView;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import com.example.eventorganizer.MainActivity;
 import com.example.eventorganizer.R;
@@ -27,13 +24,12 @@ import java.util.List;
 
 
 public class CalendarFragment extends Fragment implements AdapterView.OnItemClickListener {
-
     private TextView noEventsTextView;
     private CalendarView calendarView;
     private EventAdapter eventAdapter;
-    ListView listView;
-    View root;
-    List<Event> events = new ArrayList<>();
+    private ListView listView;
+    private View root;
+    private List<Event> events = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -45,34 +41,45 @@ public class CalendarFragment extends Fragment implements AdapterView.OnItemClic
         listView = root.findViewById(R.id.listEventsByDate);
         eventAdapter = new EventAdapter(events, this);
 
+
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+
+
+        calendarView.setDate(calendar.getTimeInMillis(), true, true);
+
+
+        showEventsForDate(year, month, dayOfMonth);
+
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                String selectedDate = year + "-" + (month + 1) + "-" + dayOfMonth;
-                events = MainActivity.dbHelper.getAllEventsByDate(selectedDate);
-
-                if (events.size() > 0) {
-                    noEventsTextView.setVisibility(View.GONE);
-                } else {
-                    noEventsTextView.setVisibility(View.VISIBLE);
-                }
-                eventAdapter.setData(events);
-                listView.setAdapter(eventAdapter);
-
-
-
+                showEventsForDate(year, month, dayOfMonth);
             }
         });
         listView.setOnItemClickListener(this);
         return root;
     }
 
+    private void showEventsForDate(int year, int month, int dayOfMonth) {
+        String selectedDate = year + "-" + (month + 1) + "-" + dayOfMonth;
+        events = MainActivity.dbHelper.getAllEventsByDate(selectedDate);
+
+        if (events.size() > 0) {
+            noEventsTextView.setVisibility(View.GONE);
+        } else {
+            noEventsTextView.setVisibility(View.VISIBLE);
+        }
+        eventAdapter.setData(events);
+        listView.setAdapter(eventAdapter);
+    }
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
         Event event = events.get(i);
         EventFragment.event = event;
         Navigation.findNavController(root).navigate(R.id.action_nav_calendar_to_eventFragment);
-
     }
 }
